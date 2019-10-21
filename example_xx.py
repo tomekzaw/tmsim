834a0aefcf094ffbac3c85defc8c6176d06bf8bc
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2019-10-21 10:13:13 +0200

    Refactor xx example

--- example_xx.py
+++ example_xx.py
@@ -6,65 +6,83 @@ import itertools
 
 TuringMachine({
     'q_s': {
-        '0': ('A', 'q_r', '->'),
-        '1': ('B', 'q_r', '->'),     
-        'A': '->',  
-        'B': '->',
-        'C': ('*', 'q_C1', '<-'),
-        'D': ('*', 'q_D1', '<-'),
-        '[]': True,
+        # replace leftmost 0 or 1 with A or B, respectively
+        '0': ('A', 'q_r', '->'), 
+        '1': ('B', 'q_r', '->'),
+        # start marking Cs and Ds with *
+        'C': 'q_*', 
+        'D': 'q_*',
+        # accept empty word as it meets the condition
+        '[]': True, 
     },
     'q_r': {
+        # find rightmost 0 or 1 by finding end of the word
         '0': '->',
         '1': '->',
-        '[]': ('q_l', '<-'),
         'C': '->',
         'D': '->',
+        # if end of the word is found, go backwards
+        '[]': ('q_l', '<-'),
     },
     'q_l': {
+        # find last 0 or 1 in whole word (that has not been replaced yet) and replace with C or D, respectively
         '0': ('C', 'q_b', '<-'),
         '1': ('D', 'q_b', '<-'),
-        'A': False,
+        # reject if there is no corresponding 0 or 1 left (if all 0s and 1s have already been replaced)
+        'A': False, 
         'B': False,
-        'C': '<-',
+        # ignore symbols that have already been replaced (Cs and Ds)
+        'C': '<-', 
         'D': '<-',
     },
-    'q_b': {
+    'q_b': { # symbol has been replaced, go back to leftmost 0 or 1
+        # ignore 0s and 1s
         '0': '<-',
         '1': '<-',
-        'A': '<-',
-        'B': '<-',
-        '[]': ('q_s', '->'),
+        # when rightmost A or B is found, leftmost 0 or 1 is on the right, now replace next pair
+        'A': ('q_s', '->'),
+        'B': ('q_s', '->'),
     },
     'q_C1': {
+        # ignore As, Bs and marked Cs
         'A': '<-',
         'B': '<-',
-        '[]': ('q_C2', '->'),
         '*': '<-',
+        # when rightmost # or left blank is found, leftmost A or B is on the right
+        '#': ('q_C2', '->'),
+        '[]': ('q_C2', '->'),
     },
     'q_D1': {
+        # ignore As, Bs and marked Cs
         'A': '<-',
         'B': '<-',
-        '[]': ('q_D2', '->'),
         '*': '<-',
+        # when rightmost # or left blank is found, leftmost A or B is on the right
+        '#': ('q_D2', '->'),
+        '[]': ('q_D2', '->'),
     },
     'q_C2': {
-        'A': ('*', 'q_*', '->'),
+        # mark corresponding A as #
+        'A': ('#', 'q_*', '->'),
+        # reject if corresponding symbol is not A
         'B': False,
-        '*': '->',
     },
     'q_D2': {
+        # mark corresponding B as #
+        'B': ('#', 'q_*', '->'),
+        # reject if corresponding symbol is not B
         'A': False,
-        'B': ('*', 'q_*', '->'),
-        '*': '->',
     },
-    'q_*': {
+    'q_*': { # find leftmost C or D
+        # ignore As, Bs and marked Cs
         'A': '->',
         'B': '->',
-        'C': 'q_s',
-        'D': 'q_s',
-        '[]': True,
         '*': '->',
+        # leftmost C or D found, now replace next pair
+        'C': ('*', 'q_C1', '<-'),
+        'D': ('*', 'q_D1', '<-'),
+        # accept if all corresponding symbols have been marked
+        '[]': True,
     }
 }).run_tests(
     inputs=(word for length in range(0, 10+1) for word in itertools.product('01', repeat=length)),
